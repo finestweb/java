c9d56d2a65050624a3548ed7defa38f6f44c917d
finestweb <info@finestweb.ru>
2019-06-07 17:05:52 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -314,8 +314,118 @@ class MyWindow extends JFrame {
     }
 }*/
 
+/*
+1. Необходимо написать два метода, которые делают следующее:
+
+1) Создают одномерный длинный массив, например:
+
+static final int size = 10000000;
+static final int h = size / 2;
+float[] arr = new float[size];
+
+2) Заполняют этот массив единицами;
+3) Засекают время выполнения: long a = System.currentTimeMillis();
+4) Проходят по всему массиву и для каждой ячейки считают новое значение по формуле:
+arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+5) Проверяется время окончания метода System.currentTimeMillis();
+6) В консоль выводится время работы: System.out.println(System.currentTimeMillis() - a);
+
+Отличие первого метода от второго:
+Первый просто бежит по массиву и вычисляет значения.
+Второй разбивает массив на два массива, в двух потоках высчитывает новые значения и потом склеивает эти массивы обратно в один.
+
+Пример деления одного массива на два:
+
+System.arraycopy(arr, 0, a1, 0, h);
+System.arraycopy(arr, h, a2, 0, h);
+
+Пример обратной склейки:
+
+System.arraycopy(a1, 0, arr, 0, h);
+System.arraycopy(a2, 0, arr, h, h);
+
+Примечание:
+System.arraycopy() – копирует данные из одного массива в другой:
+System.arraycopy(массив-источник, откуда начинаем брать данные из массива-источника, массив-назначение, откуда начинаем записывать данные в массив-назначение, сколько ячеек копируем)
+По замерам времени:
+Для первого метода надо считать время только на цикл расчета:
+
+for (int i = 0; i < size; i++) {
+arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+}
+
+Для второго метода замеряете время разбивки массива на 2, просчета каждого из двух массивов и склейки.
+
+*/
+/*
 public class Main{
     public static void main(String[] args) {
-     
+		simple();
+		hard();
+    }
+
+   	static void simple(){
+    	final int size = 100;
+		final int h = size / 2;
+		float[] arr = new float[size];
+		long start = System.currentTimeMillis();
+
+    	for(int i = 0; i < size; i++){
+			arr[i] = (float)1;
+			System.out.println(arr[i]);
+		}
+
+		for(int i = 0; i < size; i++){
+			arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+			System.out.println(arr[i]);
+		}
+
+		long end = System.currentTimeMillis() - start;
+
+		System.out.println(end);
+    }
+
+    static void hard(){
+    	final int size = 100;
+		final int h = size / 2;
+		float[] arr = new float[size];
+		float[] arr1 = new float[h];
+		float[] arr2 = new float[h];
+
+		long start2 = System.currentTimeMillis();
+
+		for(int i = 0; i < size; i++){
+			arr[i] = (float)1;
+			System.out.println(arr[i]);
+		}
+
+		System.arraycopy(arr, 0, arr1, 0, h);
+		System.arraycopy(arr, h, arr2, 0, h);
+
+		for(int k = 0; k < h; k++){
+			arr1[k] = (float)(arr1[k] * Math.sin(0.2f + k / 5) * Math.cos(0.2f + k / 5) * Math.cos(0.4f + k / 2));
+			System.out.println(arr1[k]);
+		}
+
+		for(int j = 0; j < h; j++){
+			arr2[j] = (float)(arr2[j] * Math.sin(0.2f + j / 5) * Math.cos(0.2f + j / 5) * Math.cos(0.4f + j/ 2));
+			System.out.println(arr2[j]);
+		}
+
+		//Объединение двух массивов
+
+		System.arraycopy(arr1, 0, arr, 0, h);
+		System.arraycopy(arr2, 0, arr, h, h);
+
+		long end2 = System.currentTimeMillis() - start2;
+
+		System.out.println(end2);
+
+    }
+}
+*/
+public class Main{
+    public static void main(String[] args) {
+
     }
 }
