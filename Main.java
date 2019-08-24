415d19f6ca9d2f8265e701bc9be35f1efffb2163
finestweb <info@finestweb.ru>
2019-08-25 01:55:51 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
 package com.company;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        simple();
-        //hard();
+        //simple();
+        hard();
     }
 
     static void simple(){
-        final int size2 = 1000000;
-        final int h2 = size2 / 2;
+        final int size2 = 100;
         float[] arr2 = new float[size2];
         long start2 = System.currentTimeMillis();
 
@@ -17,25 +17,43 @@ public class Main {
 
         for(int i = 0; i < size2; i++){
             arr2[i] = (float)1;
-            System.out.println(arr2[i]);
+            //System.out.println(arr2[i]);
         }
 
         //Наполнение массива случайными числами
 
         for(int i = 0; i < size2; i++){
             arr2[i] = (float)(arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            System.out.println(arr2[i]);
+            //System.out.println(arr2[i]);
         }
+
+
+
+        boolean sorted = false;
+        float temp;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < arr2.length - 1; i++) {
+                if (arr2[i] > arr2[i + 1]) {
+                    temp = arr2[i];
+                    arr2[i] = arr2[i + 1];
+                    arr2[i + 1] = temp;
+                    sorted = false;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr2));
 
         long end2 = System.currentTimeMillis() - start2;
 
         System.out.println(end2);
     }
 
-    //Разбиение массива на 2, потом воссоединение
+    //Разбиение массива на 2, потои воссоединение
 
     static void hard(){
-        final int size = 1000000;
+        final int size = 1000;
         final int h = size / 2;
         float[] arr = new float[size];
         float[] arr1 = new float[h];
@@ -54,18 +72,34 @@ public class Main {
 
         for(int k = 0; k < h; k++){
             arr1[k] = (float)(arr1[k] * Math.sin(0.2f + k / 5) * Math.cos(0.2f + k / 5) * Math.cos(0.4f + k / 2));
-            System.out.println(arr1[k]);
+            //System.out.println(arr1[k]);
         }
 
         for(int j = 0; j < h; j++){
             arr2[j] = (float)(arr2[j] * Math.sin(0.2f + j / 5) * Math.cos(0.2f + j / 5) * Math.cos(0.4f + j/ 2));
-            System.out.println(arr2[j]);
+            //System.out.println(arr2[j]);
         }
 
         //Объединение двух массивов
 
         System.arraycopy(arr1, 0, arr, 0, h);
         System.arraycopy(arr2, 0, arr, h, h);
+
+        boolean sorted = false;
+        float temp;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    sorted = false;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
 
         long end2 = System.currentTimeMillis() - start2;
 
